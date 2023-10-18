dc2ea8889722b3fb2a7f8ba8af68d84316e034db
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2023-10-18 23:27:09 +0530

    Time: 6 ms (68.81%), Space: 14.5 MB (22.80%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    
+    void bfs(int m, int n,  queue<pair <int, int> > &pixels, int color, vector<vector<int>> &image,  int pixelColor ){
+        
+        vector<pair<int, int> > directions = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        
+        while(!pixels.empty()){
+            int size = pixels.size();
+            
+                while(size--){
+                    auto [x,y] = pixels.front();
+                    pixels.pop();
+                    
+                    for(auto dir : directions){
+                        int newX = dir.first + x;
+                        int newY = dir.second + y;
+                        
+                        if(newX >= 0 && newY >= 0 && newX < m && newY < n && image[newX][newY] == pixelColor){
+                            image[newX][newY] = color;
+                            pixels.push({newX, newY});
+                        }
+                    }
+                    
+                }
+            
+            
+        }
+    }
+    
+    
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n = image[0].size();
+        
+        queue<pair <int, int> > pixels;
+        pixels.push({sr, sc});
+        
+        int pixelColor = image[sr][sc];
+        image[sr][sc] = color;
+        
+        if(pixelColor != color){
+            bfs(m, n, pixels, color, image, pixelColor);
+        }
+        
+        
+           
+        
+        
+        return image;
+    }
+};

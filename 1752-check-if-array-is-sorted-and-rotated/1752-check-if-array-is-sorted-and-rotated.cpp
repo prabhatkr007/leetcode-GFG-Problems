daf50e441468fb9784ace7fc2c512afcaebb2f89
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2023-11-13 01:40:12 +0530

    Time: 2 ms (43.35%), Space: 8.7 MB (58.12%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,12 +4,12 @@ public:
         int n = nums.size();
         int rotationCount = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
+        for (int i = 1; i < n ; i++) {
+            if (nums[i-1] > nums[i]) {
                 rotationCount++;
             }
         }
-
+        cout<<rotationCount;
         if (rotationCount > 1) {
             return false;
         }

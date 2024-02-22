f3b52e637e0996b82d5594a663a6d95714546d73
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-02-22 20:01:44 +0530

    Time: 8 ms (84.95%), Space: 10.2 MB (20.42%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+     std::vector<char> st;
+
+    for (char digit : num) {
+        while (k > 0 && !st.empty() && st.back() > digit) {
+            st.pop_back();
+            k--;
+        }
+
+        // Skip adding leading zeros
+        if (!st.empty() || digit != '0') {
+            st.push_back(digit);
+        }
+    }
+
+    while (k > 0 && !st.empty()) {
+        st.pop_back();
+        k--;
+    }
+
+    std::string result = "";
+    for (char digit : st) {
+        result += digit;
+    }
+
+    return result.empty() ? "0" : result;
+}
+    
+};

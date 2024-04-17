f48dc63257025d3abd9c6a535145cb0139b23c34
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-04-17 11:32:56 +0530

    Time: 281 ms (18.63%), Space: 196.4 MB (10.57%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,31 +1,36 @@
+#include <vector>
+#include <list>
+
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> ans;
+    std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+        std::list<int> ls;
+        std::vector<int> ans;
         
-        for(int i = 0; i < k; i++){
+        while (j < nums.size()) {
+            // Remove elements smaller than the current one from the back
+            while (!ls.empty() && ls.back() < nums[j])
+                ls.pop_back();
             
-            while(!dq.empty() && dq.back() < nums[i]){
-                dq.pop_back();
+            ls.push_back(nums[j]); // Add current element
+            
+            if (j - i + 1 < k) { 
+                j++;
+            } else { 
+                ans.push_back(ls.front()); 
+                
+                // If the element going out of the window was the maximum,
+                // remove it from the front of the list
+                if (nums[i] == ls.front())
+                    ls.pop_front();
+                
+                i++; 
+                j++; 
             }
-            
-            dq.push_back(nums[i]);
         }
         
-        ans.push_back(dq.front());
-        
-        for(int i = k; i < nums.size(); i++){
-            
-            if(dq.front() == nums[i - k]) dq.pop_front();
-            
-             while(!dq.empty() && dq.back() < nums[i]){
-                dq.pop_back();
-            }
-            
-            dq.push_back(nums[i]);
-            ans.push_back(dq.front());
-        }
         return ans;
     }
 };

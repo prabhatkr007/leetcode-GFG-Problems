75e51a5fac4542d606260be6b01600fcb57bcd85
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-05-02 08:42:20 +0530

    Time: 44 ms (69.67%), Space: 23.3 MB (74.97%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
-            return -1;
-        }
-        vector<vector<int>> dist(n, vector<int>(n, 1e9));
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, 
-                                             {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
-        
-        queue<pair<pair<int, int>, int>> q; // (coordinates, distance)
-        q.push({{0, 0}, 0}); 
-        grid[0][0] = 1; // Mark as visited
-        
-        while (!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int distance = q.front().second;
-            q.pop();
-
-            if (row == n - 1 && col == n - 1) {
-                return distance + 1;
-            }
-
-            for (const auto& dir : directions) {
-                int newr = row + dir.first;
-                int newc = col + dir.second;
-                
-                if (newr >= 0 && newr < n && newc >= 0 && newc < n && 
-                    grid[newr][newc] == 0 && distance + 1 < dist[newr][newc]) {
-                    
-                    dist[newr][newc] = distance + 1;
-                    q.push({{newr, newc}, distance + 1});
-                }
-            }
-        }
-        
+int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+    int n = grid.size();
+    if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
         return -1;
     }
+    if(n-1 == 0){
+        return 1;
+    }
+    
+    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+    
+    queue<pair<pair<int, int>, int>> q; // (coordinates, distance)
+    q.push({{0, 0}, 1}); // Starting node (row, column), distance from source
+    grid[0][0] = 1; // Mark as visited
+    
+    while (!q.empty()) {
+        int row = q.front().first.first;
+        int col = q.front().first.second;
+        int distance = q.front().second;
+        q.pop();
+        
+        if (row == n - 1 && col == n - 1) {
+            return distance;
+        }
+        
+        for (const auto& dir : directions) {
+            int new_row = row + dir.first;
+            int new_col = col + dir.second;
+            if (new_row >= 0 && new_row < n && new_col >= 0 && new_col < n && grid[new_row][new_col] == 0) {
+                grid[new_row][new_col] = 1;
+                q.push({{new_row, new_col}, distance + 1});
+            }
+        }
+    }
+    
+    return -1;
+}
 };

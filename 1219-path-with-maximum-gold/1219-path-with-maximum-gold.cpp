03ec41d8cc62efdc31ede8a3fd0e93583de8f0d2
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-05-14 11:50:21 +0530

    Time: 262 ms (28.55%), Space: 10.1 MB (36.97%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& grid, int n, int m, vector<pair<int, int>> &directions,  vector<vector<bool>> &visited) {
+    int dfs(int i, int j, vector<vector<int>>& grid, int n, int m, const vector<pair<int, int>> &directions,  vector<vector<bool>> &visited) {
         
         int curr = grid[i][j];
         visited[i][j] = true; // Mark as visited
@@ -25,7 +25,7 @@ public:
         int m = grid[0].size();
 
         int maxGold = 0;
-        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        const vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         vector<vector<bool>> visited(n, vector<bool> (m, false));
         
         for (int i = 0; i < n; i++) {

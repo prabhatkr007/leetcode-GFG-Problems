a57e5784c20c87e3ba73444e42caa7cfeaf3fc2a
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-05-14 11:48:39 +0530

    Time: 259 ms (29.21%), Space: 9.9 MB (44.61%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& grid, int n, int m, vector<pair<int, int>> &directions) {
+    int dfs(int i, int j, vector<vector<int>>& grid, int n, int m, vector<pair<int, int>> &directions,  vector<vector<bool>> &visited) {
         
         int curr = grid[i][j];
-        
-        grid[i][j] = 0; // Mark as visited
+        visited[i][j] = true; // Mark as visited
 
         int localMaxGold = 0;
         for (auto direction : directions) {
             int dx = i + direction.first;
             int dy = j + direction.second;
 
-            if (dx >= 0 && dx < n && dy >= 0 && dy < m && grid[dx][dy] != 0) {
-                localMaxGold = max(localMaxGold, dfs(dx, dy, grid, n, m, directions));
+            if (dx >= 0 && dx < n && dy >= 0 && dy < m && grid[dx][dy] != 0 && visited[dx][dy] == false) {
+                localMaxGold = max(localMaxGold, dfs(dx, dy, grid, n, m,
+                                                     directions, visited));
             }
         }
 
-        grid[i][j] = curr; // Restore original value
+        visited[i][j] = false; // mark as not visited for revisit from another path
         return curr + localMaxGold;
     }
 
@@ -26,11 +26,13 @@ public:
 
         int maxGold = 0;
         vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        vector<vector<bool>> visited(n, vector<bool> (m, false));
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] != 0) {
-                    maxGold = max(maxGold, dfs(i, j, grid, n, m,directions));
+                    maxGold = max(maxGold, dfs(i, j, grid, n, m,
+                                               directions, visited));
                 }
             }
         }

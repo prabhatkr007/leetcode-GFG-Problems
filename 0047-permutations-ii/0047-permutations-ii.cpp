7cbadf374c2c633e035dae76942fcd5788e7d3f8
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-05-21 09:44:27 +0530

    Time: 8 ms (50.74%), Space: 10.9 MB (76.77%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+    private:
+    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue; // Skip used elements
+            // Skip duplicates: if the current element is the same as the previous one and the previous one has not been used in this recursion depth
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            current.push_back(nums[i]);
+            backtrack(nums, used, current, result);
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end()); // Sort the array to handle duplicates
+        backtrack(nums, used, current, result);
+        return result;
+    }
+
+
+};

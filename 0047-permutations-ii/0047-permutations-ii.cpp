b1197b05d1d2261dc6ab532808beb70146a066b0
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-05-22 11:06:32 +0530

    Time: 7 ms (57.90%), Space: 14.2 MB (21.60%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,33 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 class Solution {
-    private:
-    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result) {
-        if (current.size() == nums.size()) {
-            result.push_back(current);
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
+       
+        if (start == nums.size()) {
+            result.push_back(nums);
             return;
         }
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (used[i]) continue; // Skip used elements
-            // Skip duplicates: if the current element is the same as the previous one and the previous one has not been used in this recursion depth
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-
-            used[i] = true;
-            current.push_back(nums[i]);
-            backtrack(nums, used, current, result);
-            current.pop_back();
-            used[i] = false;
+        unordered_set<int> used;
+        
+        for (int i = start; i < nums.size(); ++i) {
+            // If the element has already been used in this depth, skip it
+            if (used.find(nums[i]) != used.end()) continue;
+            
+            used.insert(nums[i]);
+            swap(nums[start], nums[i]);
+            backtrack(nums, start + 1, result);
+            swap(nums[start], nums[i]);
         }
     }
+
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> result;
-        vector<int> current;
-        vector<bool> used(nums.size(), false);
         sort(nums.begin(), nums.end()); // Sort the array to handle duplicates
-        backtrack(nums, used, current, result);
+        backtrack(nums, 0, result);
         return result;
     }
-
-
 };

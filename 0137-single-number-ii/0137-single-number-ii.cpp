a2bcd600d9c80a78dfd29af4e2e937141a0c3944
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-05-31 19:19:35 +0530

    Time: 8 ms (37.16%), Space: 11.8 MB (82.44%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       int result = 0;
+        for (int i = 0; i < 32; ++i) {
+            int bitCount = 0;
+            for (int num : nums) {
+                if (num & (1 << i)) {
+                    ++bitCount;
+                }
+            }
+            if (bitCount % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+        return result; 
+    }
+};

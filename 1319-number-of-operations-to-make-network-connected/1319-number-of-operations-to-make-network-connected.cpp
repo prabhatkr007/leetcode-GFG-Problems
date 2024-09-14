8bf79fac94fd3f554556b3d331cbc2e8228ee431
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-09-15 02:40:22 +0530

    Time: 118 ms (13.52%), Space: 54.9 MB (16.13%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+            visited[node] = true;
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    dfs(neighbor, adj, visited);
+                }
+            }
+        }
+    public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        // If there are fewer cables than n-1, it's impossible to connect all computers
+        if (connections.size() < n - 1) {
+            return -1;
+        }
+        
+        // Create adjacency list for the graph
+        vector<vector<int>> adj(n);
+        for (const auto& connection : connections) {
+            adj[connection[0]].push_back(connection[1]);
+            adj[connection[1]].push_back(connection[0]);
+        }
+        
+        // Count the number of connected components using DFS
+        vector<bool> visited(n, false);
+        int components = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                components++;
+                dfs(i, adj, visited);
+            }
+        }
+        
+        // The minimum number of operations needed is components - 1
+        return components - 1;
+    }
+};

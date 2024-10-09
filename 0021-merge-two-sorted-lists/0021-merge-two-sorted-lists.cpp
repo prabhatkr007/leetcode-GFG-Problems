c5f8d1930a5d080be2f017cfa0742817d1458035
Prabhat Kumar <72199660+prabhatkr007@users.noreply.github.com>
2024-10-10 00:07:24 +0530

    Time: 6 ms (48.07%), Space: 20 MB (10.87%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,54 +11,36 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-         if(!list2) return list1;
-        
-        ListNode* ptr3 = new ListNode();
-        ListNode* head = ptr3;
-        ListNode* ptr1 = list1;
-        ListNode* ptr2 = list2;
-        
-        while (ptr1 && ptr2) {
-            if (ptr1->val < ptr2->val) {
-                ptr3->val = ptr1->val; 
-                ptr1 = ptr1->next;
-            } else {
-               ptr3->val = ptr2->val; 
-                ptr2 = ptr2->next;
-                
-            }
-            
-            ptr3->next = new ListNode();
-            ptr3 = ptr3-> next;
-        }
+     
+        ListNode* dummy = new ListNode();
+        ListNode* tail = dummy;
 
-        if(ptr1){
-            while (ptr1) {
-                ptr3->val = ptr1->val; 
-                ptr1 = ptr1->next;
-                if(ptr1 == NULL) ptr3->next = NULL;
-                else{
-                    ptr3->next = new ListNode();
-                    ptr3 = ptr3-> next;
-                }
-            }
-        }
        
-
-        if(ptr2){
-            while (ptr2) {
-                ptr3->val = ptr2->val; 
-                ptr2 = ptr2->next;
-                if(ptr2 == NULL) ptr3->next = NULL;
-                else{
-                    ptr3->next = new ListNode();
-                    ptr3 = ptr3-> next;
-                }
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = new ListNode(list1->val);  
+                list1 = list1->next;
+            } else {
+                tail->next = new ListNode(list2->val); 
+                list2 = list2->next;
             }
+            tail = tail->next;
         }
 
-      
-        return head;
+        while (list1) {
+            tail->next = new ListNode(list1->val);
+            list1 = list1->next;
+            tail = tail->next;
+        }
+
+        while (list2) {
+            tail->next = new ListNode(list2->val);
+            list2 = list2->next;
+            tail = tail->next;
+        }
+
+        // Return the head of the new merged list
+        return dummy->next;
+    
     }
 };
